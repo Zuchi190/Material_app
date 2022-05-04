bdfb0d49deb7fd972b72d3ffaefbe16d84015530
Genki <g.kato@anytive.com>
2022-05-05 07:29:41 +0900

    make data_create_process

--- app/controllers/supplier_controller.rb
+++ app/controllers/supplier_controller.rb
@@ -2,4 +2,14 @@ class SupplierController < ApplicationController
   def new
     @supplier=Supplier.find_by(name:"Soga")
   end
+
+  def index
+    @suppliers=Supplier.all.order(id: "DESC")
+  end
+
+  def create
+    supplier=Supplier.new(name: params[:name],person: params[:person],phone_number: params[:phone_number])
+    supplier.save
+    redirect_to("/")
+  end
 end
