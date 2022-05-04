afb9ecc6da49949d5abbfba5b972a0da2de7dfe3
Genki <g.kato@anytive.com>
2022-05-04 23:06:52 +0900

    Make Supplier UI

--- app/controllers/supplier_controller.rb
+++ app/controllers/supplier_controller.rb
@@ -1,4 +1,5 @@
 class SupplierController < ApplicationController
   def new
+    @supplier=Supplier.find_by(name:"Soga")
   end
 end
--- db/migrate/20220430105321_create_suppliers.rb
+++ db/migrate/20220430105321_create_suppliers.rb
@@ -1,4 +1,4 @@
-class CreateSuppliers < ActiveRecord::Migration[6.1]
+r=class CreateSuppliers < ActiveRecord::Migration[6.1]
   def change
     create_table :suppliers do |t|
       t.string :name
