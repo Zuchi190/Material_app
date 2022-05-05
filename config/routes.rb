5b653b730262f1008402f99ba08b600bc02a7ec0
Genki <g.kato@anytive.com>
2022-05-05 13:29:48 +0900

    create material_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get 'material/new'
+  get 'material/new'=>"material#new"
   get 'supplier'=>"supplier#index"
   get 'supplier/new'=>"supplier#new"
   post 'supplier/create'=>"supplier#create"
+  post 'material/create'=>"material#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
