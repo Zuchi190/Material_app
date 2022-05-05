4dc70a0895b6dfadcddad896904850c2f3a54d05
Genki <g.kato@anytive.com>
2022-05-06 08:52:15 +0900

    enhance material_form

--- app/controllers/material_controller.rb
+++ app/controllers/material_controller.rb
@@ -1,12 +1,20 @@
 class MaterialController < ApplicationController
   def new
     @material=Material.new
-    @supplier=Supplier.all
   end
 
   def create
-  @material=Material.new(name: params[:name],price: params[:price],size: params[:size],supplier_id: params[:id])
-  @material.save
+  Material.create(create_params)
   redirect_to("/")
+  
+  end
+
+  def index
+    @materials=Material.all.order(id: "DESC")
+  end
+
+  private
+  def create_params
+    params.require(:material).permit(:name,:price,:size,:supplier_id)
   end
 end
